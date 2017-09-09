f4c297c616a2ec986d88da9c85f89c878c9d6105
ccabo1 <ccabo@seas.upenn.edu>
2017-09-09 00:32:25 -0400

    Readme updates

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -5,12 +5,30 @@ class Footer extends Component {
     return (
       <div className="white-blue pad-2 bord-top-blue-gray">
         <div className="container blue-gray-text">
-          <h2>
-            AppTwo
-          </h2>
-          <p>
-            Created by Cameron Cabo, Terry Jo, James Xue, and Adam Ripley.
-          </p>
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <h2>
+                AppTwo
+              </h2>
+              <p>
+                Created by Cameron Cabo, Terry Jo, James Xue, and Adam Ripley.
+              </p>
+            </div>
+
+            <div className="col-12 col-md-4">
+              <p className="bold">
+                Navigation
+              </p>
+              <ul className="clean-list">
+                <li>
+                  <a href="/" className="blue-gray-text">Home</a>
+                </li>
+                <li>
+                  <a href="/about" className="blue-gray-text">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     );
