e97a1bd8883bb32d9bb62aea77e145fd8c757182
James Xue <email@jxue.co>
2017-09-09 09:47:11 -0400

    prettier

--- src/modules/applications/Application.js
+++ src/modules/applications/Application.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Application extends Component {
   render() {
     return (
       <div className="row">
         <div className="col-12">
-          <h2>
-            Job title
-          </h2>
-          <p>
-            Company name
-          </p>
-          <p>
-            Location
-          </p>
-          <p>
-            Status
-          </p>
+          <h2>Job title</h2>
+          <p>Company name</p>
+          <p>Location</p>
+          <p>Status</p>
         </div>
       </div>
     );
--- src/modules/auth/Register.js
+++ src/modules/auth/Register.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import ResCol from '../helper/ResCol';
+import ResCol from "../helper/ResCol";
 
 import React, { Component } from "react";
 
@@ -17,8 +17,8 @@ class Register extends Component {
 
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        window.location = '/';
+      if (user) {
+        window.location = "/";
       }
     });
   }
@@ -52,9 +52,7 @@ class Register extends Component {
         <form onSubmit={this.handleSubmit}>
           <h2 className="marg-bot-1">Register</h2>
 
-          <label>
-            Email
-          </label>
+          <label>Email</label>
           <input
             type="email"
             className="form-control marg-bot-1"
@@ -63,9 +61,7 @@ class Register extends Component {
             required="true"
           />
 
-          <label>
-            Password
-          </label>
+          <label>Password</label>
           <input
             type="password"
             className="form-control marg-bot-2"
@@ -73,10 +69,17 @@ class Register extends Component {
             onChange={this.handleChangePassword}
             required="true"
           />
-          <input type="submit" className="btn purple bold shade-3 hover white-text bold" value="Submit" />
+          <input
+            type="submit"
+            className="btn purple bold shade-3 hover white-text bold"
+            value="Submit"
+          />
 
           <p className="marg-top-1 blue-gray-text">
-            Already have an account? <a href="./login" className="purple-text">Log in here.</a>
+            Already have an account?{" "}
+            <a href="./login" className="purple-text">
+              Log in here.
+            </a>
           </p>
         </form>
       </ResCol>
