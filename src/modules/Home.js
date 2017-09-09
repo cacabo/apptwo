191e144b200a36b2a6f80620a5ab0e230ae53a12
James Xue <email@jxue.co>
2017-09-09 02:00:20 -0400

    logout

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import firebase from "firebase";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        window.location = "/login";
+      }
+    });
+  }
   render() {
     return (
       <div className="row">
         <div className="col-12">
-          <h2>
-            AppTwo
-          </h2>
+          <h2>AppTwo</h2>
           <p>
-            An application for applications. We make applying for jobs, clubs, and positions easier by organizing your information and allowing you to find the details that matter. Understand how far you are in each application, how much work there is to get done on your application, and who you've been put in contact with all in one place.
+            An application for applications. We make applying for jobs, clubs,
+            and positions easier by organizing your information and allowing you
+            to find the details that matter. Understand how far you are in each
+            application, how much work there is to get done on your application,
+            and who you've been put in contact with all in one place.
           </p>
-          <p>
-            Say goodbye to a map of excel sheets and word docs.
-          </p>
+          <p>Say goodbye to a map of excel sheets and word docs.</p>
         </div>
       </div>
     );
--- src/modules/navbar/NavbarNavigation.js
+++ src/modules/navbar/NavbarNavigation.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import firebase from "firebase";
 
 class NavbarNavigation extends Component {
+  logout() {
+    console.log("logging you out!");
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful
+      })
+      .catch(error => {
+        // An error happened.
+        alert(error.message);
+      });
+  }
   render() {
     return (
       <ul className="navbar-nav">
         <li className="nav-item bold">
-          <a className="nav-link" href="/">Home</a>
+          <a className="nav-link" href="/">
+            Home
+          </a>
         </li>
         <li className="nav-item bold">
-          <a className="nav-link" href="/about">About</a>
+          <a className="nav-link" href="/about">
+            About
+          </a>
+        </li>
+        <li className="nav-item bold">
+          <a className="nav-link" onClick={() => this.logout()}>
+            Logout
+          </a>
         </li>
       </ul>
     );
