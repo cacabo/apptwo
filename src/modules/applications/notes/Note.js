4e92d2e6a9e079da03071723f747258c8127ffc3
ccabo1 <ccabo@seas.upenn.edu>
2017-09-10 07:33:30 -0400

    better Note

--- src/modules/applications/notes/Note.js
+++ src/modules/applications/notes/Note.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import $ from 'jquery';
 
 class Note extends Component {
+  componentDidMount() {
+    $('.fade-in').delay(100).fadeIn(200);
+  }
+  
   render() {
     return (
       <div className="fade-in white pad-2 round-1 shade-1">
