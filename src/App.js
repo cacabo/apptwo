91b92a6750a3f5cbb3eb46152139a14201cf9d7c
ccabo1 <ccabo@seas.upenn.edu>
2017-09-10 00:32:01 -0400

    Route for edit application

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import firebase from "firebase";
 import { firebaseApp } from "./firebase.js";
+import $ from 'jquery'
 
+// CSS
 import "./css/strapper.min.css";
 import "./css/App.css";
 
@@ -19,7 +21,7 @@ import Register from "./modules/auth/Register";
 import NotFound from "./modules/NotFound";
 import User from "./modules/users/User";
 import NewApplication from "./modules/applications/NewApplication";
-import $ from 'jquery'
+import EditApplication from "./modules/applications/EditApplication";
 
 // Application
 class App extends Component {
@@ -35,34 +37,6 @@ class App extends Component {
       this.setState({ isLoggedIn: !!user });
     });
   }
-
-  // <PageContainer>
-  //   {
-  //     this.state.isLoggedIn ? (
-  //       <Switch>
-  //         <Route exact path="/" component={() => (<Home isLoggedIn={true} />)} />
-  //         <Route exact path="/about" component={About} />
-  //         <Route exact path="/user" component={User} />
-  //         <Route exact path="/applications" component={Table} />
-  //         <Route exact path="/applications/new" component={NewApplication} />
-  //         <Route exact path="/applications/:id" component={Application} />
-  //         <Route exact path="/register" component={() => {}} />
-  //         <Route exact path="/login" component={() => {}} />
-  //         <Route exact path="/404" component={NotFound} />
-  //         <Route path='*' exact={true} component={NotFound} />
-  //       </Switch>
-  //     ) : (
-  //       <Switch>
-  //         <Route exact path="/" component={() => (<Home isLoggedIn={false} />)} />
-  //         <Route exact path="/about" component={About} />
-  //         <Route exact path="/register" component={Register} />
-  //         <Route exact path="/login" component={Login} />
-  //         <Route exact path="/404" component={NotFound} />
-  //         <Route path='*' exact={true} component={NotFound} />
-  //       </Switch>
-  //     )
-  //   }
-  // </PageContainer>
 
   routes() {
     console.log('in routes');
@@ -80,6 +54,7 @@ class App extends Component {
             <Route exact path="/user" component={User} />
             <Route exact path="/applications" component={Table} />
             <Route exact path="/applications/new" component={NewApplication} />
+            <Route exact path="/applications/:id/edit" component={NewApplication} />
             <Route exact path="/applications/:id" component={Application} />
             <Route exact path="/register" component={() => {}} />
             <Route exact path="/login" component={() => {}} />
